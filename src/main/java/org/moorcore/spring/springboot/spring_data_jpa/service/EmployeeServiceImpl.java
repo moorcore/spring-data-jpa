86e78b22fc326557b12f3f63cc7a4df955889ecd
Maxim <moorcore@users.noreply.github.com>
2021-05-08 14:10:46 +0200

    init commit

---
+++ src/main/java/org/moorcore/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package org.moorcore.spring.springboot.spring_data_jpa.service;
+
+import org.moorcore.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import org.moorcore.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()) {
+            employee = optionalEmployee.get();
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String name) {
+        return employeeRepository.findAllByName(name);
+    }
+}
